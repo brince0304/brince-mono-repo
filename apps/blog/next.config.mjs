a77fd4e7f5441c16aaf6c0516aa19e00a7f5a97f
brince0304 <brince0304@gmail.com>
2024-08-23 01:27:25 +0900

    chore: 넥스트 설정 변경

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
 	images: {
 		domains: ["github.com", "api.dicebear.com"],
 	},
+	env: {
+		NOTION_API_KEY: process.env.NOTION_API_KEY,
+		NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+		NOTION_COMMENT_DATABASE_ID: process.env.NOTION_COMMENT_DATABASE_ID,
+		NOTION_OWNER_ID: process.env.NOTION_OWNER_ID,
+	},
 };
 
 export default nextConfig;
