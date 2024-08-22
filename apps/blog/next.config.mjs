e6239575eba3498a309a56deb6966e7afefd5d70
brince0304 <brince0304@gmail.com>
2024-08-23 00:15:16 +0900

    chore: vercel 설정 수정

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['github.com', 'api.dicebear.com'],
-  },
+	images: {
+		domains: ["github.com", "api.dicebear.com"],
+	},
+	transpilePackages: [
+		"@brince-mono-repo/shared-components",
+		"@brince-mono-repo/shared-styles",
+	],
 };
 
 export default nextConfig;
