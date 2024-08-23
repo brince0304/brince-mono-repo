ac95cc5608daecc8b65d59200a332b3514c33095
brince0304 <brince0304@gmail.com>
2024-08-23 12:27:01 +0900

    chore: 환경변수 수정

--- apps/blog/src/lib/notion/consts.ts
+++ apps/blog/src/lib/notion/consts.ts
@@ -1,3 +1,3 @@
-export const COMMENT_DATABASE_ID = process.env.NEXT_PUBLIC_NOTION_COMMENT_DATABASE_ID as string;
-export const POST_DATABASE_ID = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID as string;
-export const NOTION_TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN as string;
+export const COMMENT_DATABASE_ID = process.env.NOTION_COMMENT_DATABASE_ID as string;
+export const POST_DATABASE_ID = process.env.NOTION_DATABASE_ID as string;
+export const NOTION_TOKEN = process.env.NOTION_TOKEN as string;
