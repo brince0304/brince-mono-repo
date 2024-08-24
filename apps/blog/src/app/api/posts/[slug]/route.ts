55202f59de284ce0e8be4c5bedc04ded5d471255
brince0304 <brince0304@gmail.com>
2024-08-25 01:04:25 +0900

    refactor: 댓글, 게시글 서버 API 수정

--- apps/blog/src/app/api/posts/[slug]/route.ts
+++ apps/blog/src/app/api/posts/[slug]/route.ts
@@ -1,18 +1,59 @@
-import { type NextRequest, NextResponse } from 'next/server';
-import { notionClient } from '@/lib/notion/notion';
-export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
-  const slug = params.slug;
+import { notionClient } from "@/lib/notion/notion";
+import type { NotionProperties } from "@/models/notion";
+import { cookies } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
+export async function GET(
+	req: NextRequest,
+	{ params }: { params: { slug: string } },
+) {
+	req.headers.set(
+		"Cache-Control",
+		"public, s-maxage=60, stale-while-revalidate=30",
+	);
 
-  if (!slug) {
-    return NextResponse.json({ error: '페이지 ID가 존재하지 않습니다.' }, { status: 400 });
-  }
+	const slug = params.slug;
 
-  try {
-    const response = await notionClient.getPageBySlug(slug);
+	if (!slug) {
+		return NextResponse.json(
+			{ error: "페이지 ID가 존재하지 않습니다." },
+			{ status: 400 },
+		);
+	}
 
-    return NextResponse.json(response);
-  } catch (error) {
-    console.error('Error fetching post:', error);
-    return NextResponse.json({ error: '게시글 조회에 실패하였습니다.' }, { status: 500 });
-  }
+	try {
+		const response = await notionClient.getPageBySlug(slug);
+
+		const cookieStore = cookies();
+		const viewedCookie = cookieStore.get(`${slug}-viewed`);
+
+		if (viewedCookie) {
+			return NextResponse.json(response);
+		}
+
+		// 쿠키 설정 (24시간 유효)
+		cookieStore.set(`${slug}-viewed`, "true", {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+			maxAge: 60 * 60 * 24, // 24시간
+			path: "/",
+		});
+
+		await notionClient.updatePostProperties(response?.page.id, <
+			NotionProperties
+		>{
+			Views: {
+				type: "number",
+				number: (response?.page?.properties?.Views?.number ?? 0) + 1,
+			},
+		});
+
+		return NextResponse.json(response);
+	} catch (error) {
+		console.error("Error fetching post:", error);
+		return NextResponse.json(
+			{ error: "게시글 조회에 실패하였습니다." },
+			{ status: 500 },
+		);
+	}
 }
