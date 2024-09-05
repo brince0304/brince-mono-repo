7868edf02c040ea27ef5133e8e577bc4f3364965
brince0304 <brince0304@gmail.com>
2024-09-05 21:48:28 +0900

    feat: ProfileCard 컴포넌트 구현

---
+++ packages/shared-components/src/components/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,37 @@
+// ProfileCard.stories.js
+import React from "react";
+import ProfileCard from "./ProfileCard";
+
+export default {
+	title: "Components/ProfileCard",
+	component: ProfileCard,
+	parameters: {
+		layout: "centered",
+	},
+};
+
+export const Default = () => <ProfileCard />;
+
+export const Mobile = () => (
+	<div className="max-w-sm">
+		<ProfileCard />
+	</div>
+);
+
+export const Desktop = () => (
+	<div className="max-w-4xl">
+		<ProfileCard />
+	</div>
+);
+
+Mobile.parameters = {
+	viewport: {
+		defaultViewport: "mobile1",
+	},
+};
+
+Desktop.parameters = {
+	viewport: {
+		defaultViewport: "desktop",
+	},
+};
