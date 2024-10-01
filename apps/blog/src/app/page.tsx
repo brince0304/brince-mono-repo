77831f78ad0b715aa879c815b06d3f14157c1917
brince0304 <brince0304@gmail.com>
2024-10-01 16:01:33 +0900

    chore: 테스트용으로 force-dynamic 키워드 메인 페이지 컴포넌트에 추가

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Posts from '@/components/Posts/Posts';
 import { ProfileCard } from '@repo/ui/ProfileCard';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   return (
     <main className={'mx-auto'}>
