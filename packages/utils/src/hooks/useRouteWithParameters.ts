3ec1433efb2a8d7e77bd01561881cbbc1b116428
brince0304 <brince0304@gmail.com>
2024-10-06 00:18:44 +0900

    feat: useRouteWithParameters 훅 정의 및 패키지 추가

--- packages/utils/src/hooks/useRouteWithParameters.ts
+++ packages/utils/src/hooks/useRouteWithParameters.ts
@@ -1,15 +1,50 @@
-import { isServer } from '@/lib/next';
-import { useRouter } from 'next/navigation';
-import { useMemo } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+type Parameter = {
+  [key: string]: string;
+};
 
 type RouteWithParameters = {
-  parameters: Record<string, string>;
+  parameters?: Parameter;
+  baseUrl?: string;
   replace?: boolean;
-  maintenance?: boolean;
+  clear?: boolean;
 };
 
-const useRouteWithParameters = (args: RouteWithParameters) => {
-  const router = isServer() ? useRouter() : window.location;
+const useRouteWithParameters = () => {
+  const router = useRouter();
+  const pathname = usePathname();
 
-  const { parameters, replace, maintenance } = args;
+  const handleRoute = ({ baseUrl, parameters, replace, clear = true }: RouteWithParameters) => {
+    const currentSearchParams = new URLSearchParams(useSearchParams());
+    const newSearchParams = new URLSearchParams(currentSearchParams);
+
+    if (parameters) {
+      for (const [key, value] of Object.entries(parameters)) {
+        newSearchParams.set(key, value);
+      }
+
+      if (clear) {
+        newSearchParams.forEach((_, key) => {
+          if (!(key in parameters)) {
+            newSearchParams.delete(key);
+          }
+        });
+      }
+    }
+
+    const queryString = newSearchParams.toString();
+    const newPathname = `${baseUrl || pathname}${queryString ? `?${queryString}` : ''}`;
+
+    if (replace) {
+      router.replace(newPathname);
+      return;
+    }
+
+    router.push(newPathname);
+  };
+
+  return handleRoute;
 };
+
+export default useRouteWithParameters;
