a68e5d346282616ad3a8b6e5e4145d83c544335a
brince0304 <brince0304@gmail.com>
2024-11-24 17:46:00 +0900

    style: 검색 창 디자인 수정

--- apps/blog/src/app/posts/_components/PostListHeader.tsx
+++ apps/blog/src/app/posts/_components/PostListHeader.tsx
@@ -6,13 +6,13 @@ import PostCategorySelector from './PostCategorySelector';
 
 const PostListHeader = () => {
   return (
-    <div className="sticky top-[56px] z-10 backdrop-blur-sm py-4 border-b border-border bg-background flex flex-col gap-4">
-      <div className="flex space-x-4">
+    <div className="sticky top-[56px] z-10 backdrop-blur-sm py-4 border-b border-border bg-background flex flex-col gap-4 -mx-4">
+      <div className="flex space-x-4 px-4">
         <PostSearch />
         <PostCategorySelector />
       </div>
 
-      <div className="flex space-x-2">
+      <div className="flex space-x-2 px-4">
         <PostTags />
       </div>
     </div>
