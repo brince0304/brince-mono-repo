2f288ca16079506c5241010525557128217eeaff
brince0304 <brince0304@gmail.com>
2024-12-02 22:11:14 +0900

    chore: 테스트 유틸 정의

--- apps/blog/src/test/testUtils.tsx
+++ apps/blog/src/test/testUtils.tsx
@@ -1,7 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, type RenderResult } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export const renderWithQuery = (component: React.ReactNode) => {
+export const renderWithQuery = (component: React.ReactNode): RenderResult => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
