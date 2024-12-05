a1f3c3aa56fc61e4d4291251a06e08486fbb1926
brince0304 <brince0304@gmail.com>
2024-12-06 08:58:44 +0900

    chore: 옵션 수정

--- apps/blog/src/app/posts/_components/PostListHeader.tsx
+++ apps/blog/src/app/posts/_components/PostListHeader.tsx
@@ -3,9 +3,10 @@
 import PostTags from './PostTags';
 import PostSearch from './PostSearchBox';
 import PostCategorySelector from './PostCategorySelector';
+import { wrap } from '@suspensive/react';
 
-const PostListHeader = () => {
-  return (
+const PostListHeader = wrap.Suspense().on(
+  () => (
     <div className="sticky top-[56px] z-10 backdrop-blur-sm py-4 border-b border-border bg-background/95 flex flex-col gap-4 -mx-4">
       <div className="flex space-x-4 px-4">
         <PostSearch />
@@ -16,7 +17,7 @@ const PostListHeader = () => {
         <PostTags />
       </div>
     </div>
-  );
-};
+  )
+);
 
 export default PostListHeader;
