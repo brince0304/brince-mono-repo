16aba8385eb9109204d811e34d0130ee32d29680
brince0304 <brince0304@gmail.com>
2024-12-05 21:52:15 +0900

    chore: static 설정 제거

--- apps/blog/src/app/posts/[slug]/page.tsx
+++ apps/blog/src/app/posts/[slug]/page.tsx
@@ -37,7 +37,6 @@ export async function generateStaticParams() {
   }));
 }
 
-export const dynamic = 'force-static';
 export const revalidate = 3600;
 
 export default async function Post({ params }: { params: { slug: string } }) {
