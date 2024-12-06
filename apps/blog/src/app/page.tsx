76e6e47e4b070d8833873513cfd6903086191434
brince0304 <brince0304@gmail.com>
2024-12-06 13:10:58 +0900

    improvement: 메인 페이지 프리패치를 통해 포스트 탭으로 넘어가도 다시 안불러오도록 변경

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -1,9 +1,9 @@
 import { generateHomeMetadata } from '@/lib/meta';
 import { ProfileCard } from '@/app/_components/ProfileCard';
 import type { Metadata } from 'next';
-import { notionClient } from '@/lib/notion/notion';
-import PostList from './_components/PostList';
-import { TextGrid } from '@repo/ui/TextGrid';
+import PostSection from './_components/PostSection';
+import QueryHydrationBoundary from '@/components/QueryHydrationBoundary';
+import { PostQueryOptions } from '@/hooks/post';
 
 export const generateMetadata = async (): Promise<Metadata> => {
   return generateHomeMetadata();
@@ -12,15 +12,12 @@ export const generateMetadata = async (): Promise<Metadata> => {
 export const dynamic = 'force-static';
 
 export default async function HomePage() {
-  const posts = await notionClient.getPosts();
-
   return (
     <main className={'flex flex-col mx-auto gap-4 min-h-screen'}>
       <ProfileCard />
-      <section className={'flex flex-col gap-4'}>
-        <TextGrid title="최근 포스트" description="여러 이야기를 다루고 있어요 🤗" />
-        {posts && <PostList posts={posts} />}
-      </section>
+      <QueryHydrationBoundary queryOptions={PostQueryOptions.getPrefetchPosts()}>
+        <PostSection />
+      </QueryHydrationBoundary>
     </main>
   );
 }
